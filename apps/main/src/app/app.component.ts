48631e53117a2608bda6ab576524236bff25ead8
DXF0530 <dmitry.fleish@sbdinc.com>
2023-03-02 16:29:57 +0200

    Initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-dates-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
